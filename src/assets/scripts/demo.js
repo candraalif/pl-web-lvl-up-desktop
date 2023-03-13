7694554538b75aa1b767566ef0d59f80c1782af9
candraalif <candra.irawan@traveloka.com>
2023-03-13 09:33:29 +0700

    create dynamic header

--- src/assets/scripts/demo.js
+++ src/assets/scripts/demo.js
@@ -1,1 +1,11 @@
-console.log('Hola!')
+console.log('Hola hmm!')
+
+$(window).on('scroll', function (){
+  var windowpos = $(window).scrollTop()
+  console.log(windowpos)
+  if( windowpos >= 80 ) {
+       $('#header').addClass('active')
+  } else {
+       $('#header').removeClass('active')
+  }
+});
