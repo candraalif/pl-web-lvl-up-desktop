ab078c607793bfef1a8180340e0edf411070a9b3
candraalif <candra.irawan@traveloka.com>
2023-03-15 16:44:15 +0700

    create footer & jumbotron

--- src/assets/scripts/demo.js
+++ src/assets/scripts/demo.js
@@ -1,8 +1,15 @@
 $(window).on('scroll', function (){
-  var windowpos = $(window).scrollTop()
-  if( windowpos >= 80 ) {
-       $('#header').addClass('active')
-  } else {
-       $('#header').removeClass('active')
-  }
+	var windowpos = $(window).scrollTop()
+	if( windowpos >= 80 ) {
+		$('#header').addClass('active')
+	} else {
+		$('#header').removeClass('active')
+	}
 });
+
+$(document).on('ready', function(){
+	var jumbo = $('.jumbotron')
+	var jumboImages = jumbo.find('.bg-image > .bg-image-item').attr('src')
+	jumbo.css('background-image','url('+jumboImages+')')
+	console.log('Image Bg = '+ jumboImages)
+})
