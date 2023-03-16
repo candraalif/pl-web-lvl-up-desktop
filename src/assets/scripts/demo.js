42bc916fef15a511a92c6c2ba03af227e22771f3
candraalif <candra.irawan@traveloka.com>
2023-03-16 11:27:58 +0700

    updating jumbotron

--- src/assets/scripts/demo.js
+++ src/assets/scripts/demo.js
@@ -8,8 +8,21 @@ $(window).on('scroll', function (){
 });
 
 $(document).on('ready', function(){
-	var jumbo = $('.jumbotron')
-	var jumboImages = jumbo.find('.bg-image > .bg-image-item').attr('src')
-	jumbo.css('background-image','url('+jumboImages+')')
-	console.log('Image Bg = '+ jumboImages)
+	$('.jumbotron .bg-image .bg-image-item').each(function() {
+		let bg = $(this).data('image-bg')
+		$(this).css('background-image', 'url('+bg+')')
+	})
+	
+  var i = 0;
+	var jumboImages = $('.jumbotron .bg-image .bg-image-item')
+	console.log(jumboImages[0])
+	// set initial background-image
+	jumboImages.eq(0).css('opacity', 1 )
+	// change image every after 5 seconds
+  setInterval(function(){
+		jumboImages.css('opacity', 0 )
+    jumboImages.eq(i++).css('opacity', 1 )
+    if(i == jumboImages.length)
+      i = 0
+  }, 5000)
 })
