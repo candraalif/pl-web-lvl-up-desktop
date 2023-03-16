9d3315f792d9cbbc2cf25e49e254f694b86bebd2
candraalif <candra.irawan@traveloka.com>
2023-03-17 00:11:07 +0700

    bug fixing on safari & mobile browsers

--- src/assets/scripts/demo.js
+++ src/assets/scripts/demo.js
@@ -7,36 +7,35 @@ $(window).on('scroll', function (){
 	}
 });
 
-$(document).on('ready', function(){
-	$('.jumbotron .bg-image .bg-image-item').each(function() {
-		let bg = $(this).data('image-bg')
-		$(this).css('background-image', 'url('+bg+')')
-	})
-	// Run Image Slider
-	function nextBgImage(){
-		var items = $('.jumbotron .bg-image .bg-image-item')
-		var activeItem = $('.jumbotron .bg-image .bg-image-item.active')
-		var nextItem = activeItem.next()
-		if(activeItem.is(':last-child'))
-			var nextItem = items.eq(0)
-		activeItem.removeClass('active')
-		nextItem.addClass('active')
-	}
-	function prevBgImage(){
-		var items = $('.jumbotron .bg-image .bg-image-item')
-		var activeItem = $('.jumbotron .bg-image .bg-image-item.active')
-		var prevItem = activeItem.prev()
-		if(activeItem.is(':first-child'))
-			var prevItem = items.last()
-		activeItem.removeClass('active')
-		prevItem.addClass('active')
-	}
-	// Set Image Change Interval (in milisecond, 10000 = 10s)
-	setInterval(function(){ nextBgImage() }, 10000)
-  $('.bg-image-control-prev').on('click', function(){
-		prevBgImage()
-	})
-	$('.bg-image-control-next').on('click', function(){
-		nextBgImage()
-	})
+
+$('.jumbotron .bg-image .bg-image-item').each(function() {
+	let bg = $(this).data('image-bg')
+	$(this).css('background-image', 'url('+bg+')')
+})
+// Run Image Slider
+function nextBgImage(){
+	var items = $('.jumbotron .bg-image .bg-image-item')
+	var activeItem = $('.jumbotron .bg-image .bg-image-item.active')
+	var nextItem = activeItem.next()
+	if(activeItem.is(':last-child'))
+		var nextItem = items.eq(0)
+	activeItem.removeClass('active')
+	nextItem.addClass('active')
+}
+function prevBgImage(){
+	var items = $('.jumbotron .bg-image .bg-image-item')
+	var activeItem = $('.jumbotron .bg-image .bg-image-item.active')
+	var prevItem = activeItem.prev()
+	if(activeItem.is(':first-child'))
+		var prevItem = items.last()
+	activeItem.removeClass('active')
+	prevItem.addClass('active')
+}
+// Set Image Change Interval (in milisecond, 10000 = 10s)
+setInterval(function(){ nextBgImage() }, 10000)
+$('.bg-image-control-prev').on('click', function(){
+	prevBgImage()
+})
+$('.bg-image-control-next').on('click', function(){
+	nextBgImage()
 })
